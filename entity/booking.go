3ec9f1a3f0d0cd9712ea046aeaab5945aff6d999
Vinzryyy <148874643+Vinzryyy@users.noreply.github.com>
2026-02-06 08:53:40 +0700

    fix  menu fitur

--- entity/booking.go
+++ entity/booking.go
@@ -1,1 +1,14 @@
 package entity
+
+import "time"
+
+type Booking struct {
+	BookingID  int
+	UserID     int
+	CourtID    int
+	TimeSlotID int
+	BookingDate time.Time
+	TotalPrice float64
+	Status     string
+	CreatedAt time.Time
+}
--- entity/court.go
+++ entity/court.go
@@ -1,1 +1,9 @@
 package entity
+
+type Court struct {
+	CourtID      int
+	CourtName    string
+	Location     string
+	PricePerHour float64
+	Status       string
+}
